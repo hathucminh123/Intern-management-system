3029c042d5d97ad53f990bf19b8693883fe593ff
hathucminh123 <hathucminh456@gmail.com>
2024-05-13 22:58:59 +0700

    update signupPage

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -268,9 +268,7 @@ const SignupPage = () => {
   Intern
 </h1>
       <h4>Chào mừng đến với Intern management system</h4>
-      <h1 className=" text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
+    
     </WrapperContainerRight>
   </div>
 </div >
