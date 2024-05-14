e5cbf2c6e9374ce92b8d0d01c44dd70193778bc9
trinhtam <trinhtam2001@gmail.com>
2024-05-14 22:35:24 +0700

    Shedule component

--- src/components/InternDashboard/InternDashboard.jsx
+++ src/components/InternDashboard/InternDashboard.jsx
@@ -1,6 +1,6 @@
 import TaskTable from "../InternTaskTable/InternTaskTable";
-import { Col, Row, Card, Flex, Typography, Progress, } from 'antd';
-import { SyncOutlined, ClockCircleOutlined, CheckCircleOutlined } from '@ant-design/icons';
+import { Col, Row, Card, Flex, Typography, Progress, List, Avatar, } from 'antd';
+import { SyncOutlined, ClockCircleOutlined, CheckCircleOutlined, FieldTimeOutlined } from '@ant-design/icons';
 
 const InternDashBoard = () => {
 
@@ -43,6 +43,25 @@ const InternDashBoard = () => {
         </Card>
     );
 
+    const scheduleData = [
+        {
+            time: '10AM',
+            title: 'Research',
+        },
+        {
+            time: '2PM',
+            title: 'Grooming Meeting',
+        },
+        {
+            time: '4PM',
+            title: 'Team Standup',
+        },
+        {
+            time: '6PM',
+            title: 'Project Review',
+        },
+    ];
+
     return (
         <>
             <Typography.Title level={2} strong>
@@ -80,11 +99,28 @@ const InternDashBoard = () => {
                 <Col span={8}>
                     <div>
 
-                        <Card style={{ height: 260, width: '100%', }}>
+                        <Card style={{ height: 300, width: '100%', marginBottom: '20px' }}>
                             <Typography.Title level={2} strong>
                                 Task Progress
                             </Typography.Title>
                             <Progress style={{ display: 'flex', justifyContent: 'center' }} size={200} type="circle" percent={percent} format={(percent) => `${percent}% Complete`} />
+                        </Card>
+                        <Card style={{ width: '100%', }}>
+                            <Typography.Title level={2} strong>
+                                Shedule's Today
+                            </Typography.Title>
+                            <List
+                                itemLayout="horizontal"
+                                dataSource={scheduleData}
+                                renderItem={(item, index) => (
+                                    <List.Item style={{ background: index % 2 === 0 ? '#f9f9f9' : '#e6f7ff', padding: '10px 20px' }}>
+                                        <List.Item.Meta
+                                            avatar={<FieldTimeOutlined />}
+                                            title={<a href="">{item.time} - {item.title}</a>}
+                                        />
+                                    </List.Item>
+                                )}
+                            />
                         </Card>
                     </div>
                 </Col>
